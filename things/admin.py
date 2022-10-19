dcdf3d0711043c83bd0a0906162af37f74daa9be
Jory AlJuhani <jory.aljuhani@kcl.ac.uk>
2022-10-19 16:01:13 +0100

    Things Engagment

--- things/admin.py
+++ things/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# from .models import Thing
+#
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     """Configuration of the admin interface for users."""
+#     list_display=[
+#     'name','description','quantity',
+#     ]
+# # Register your models here.
--- things/models.py
+++ things/models.py
@@ -1,3 +1,16 @@
+from django.core.validators  import MinValueValidator,MaxValueValidator
+
 from django.db import models
+from wsgiref.validate import validator
+
+class Thing(models.Model):
+
+
+    name=models.CharField(max_length=30,unique=True, blank=False)
+    description=models.CharField(max_length=120, blank=True)
+    quantity=models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
+
+
+
 
 # Create your models here.
